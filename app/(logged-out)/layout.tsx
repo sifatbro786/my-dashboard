dbca7af7bedff30cdbaaf549771831086efb3e81
sfatbro786 <solaimanislamsifat@gmail.com>
2025-08-28 01:08:34 +0600

    build the landing page

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,14 @@
+import { LightDarkToggle } from "@/components/light-dark-toggle";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function LoggedOutLayout({ children }: Props) {
+    return (
+        <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
+            {children}
+            <LightDarkToggle />
+        </div>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -19,8 +20,17 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={cn(poppins.className, "dark")}>{children}</body>
+        <html lang="en" suppressHydrationWarning>
+            <body className={cn(`${poppins.className} antialiased`)}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
